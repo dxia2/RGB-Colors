662c81f53d18172c5b791479a2ff11511d4887c8
dxia2 <90279328+dxia2@users.noreply.github.com>
2021-09-13 13:05:09 -0600

    Add files via upload

--- rgb-colors/script.js
+++ rgb-colors/script.js
@@ -1,16 +1,53 @@
 // rgb colors with js by david
 
+let previewBtnEl = document.getElementById("preview-btn");
+let redInEl = document.getElementById("redIn");
+let greenInEl = document.getElementById("greenIn");
+let blueInEl = document.getElementById("blueIn");
+let rgbStringOut = document.getElementById("rgbStrOut");
+let displayEl = document.getElementById("display");
 // event listener
-document.getElementById("preview-btn").addEventListener("click", rgbPreview);
+redInEl.addEventListener("input", rgbPreview);
+greenInEl.addEventListener("input", rgbPreview);
+blueInEl.addEventListener("input", rgbPreview);
+
 
 function rgbPreview(){
     //input
-    let rValue = document.getElementById("redIn").value;
-    let gValue = document.getElementById("greenIn").value;
-    let bValue = document.getElementById("blueIn").value;
+    let rValue = +redInEl.value;
+    let gValue = +greenInEl.value;
+    let bValue = +blueInEl.value;
+
+    //validate colors (constrain colors between 0-255)
+    //check rValue
+    if(rValue < 0){
+        rValue = 0;
+        redInEl.value = 0;
+    }else if(rValue > 255){
+        rValue = 255;
+        redInEl.value = 255;
+    }
+
+    if(gValue < 0){
+        gValue = 0;
+        greenInEl.value = 0;
+    }else if(gValue > 255){
+        gValue = 255;
+        greenInEl.value = 255;
+    }
+
+    if(bValue < 0){
+        bValue = 0;
+        blueInEl.value = 0;
+    }else if(bValue > 255){
+        bValue = 255;
+        blueInEl.value = 255;
+    }
+
     //process
     let rgbString = "rgb(" + rValue + ", " + gValue + ", " + bValue + ")";
     //output
-    document.getElementById("rgbStrOut").innerHTML = rgbString;
-    document.getElementById("display").style.background = rgbString;
+    rgbStringOut.innerHTML = rgbString;
+    displayEl.style.background = rgbString;
+
 }
